807cf555b853b326e0b24a11655f420527b6a8bb
emilioriosvz <emilioriosvz@gmail.com>
2020-09-27 19:35:49 +0200

    💅 install and configure prettier with eslint

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-import { config } from 'dotenv'
-config()
+import { config } from "dotenv";
+config();
 
-export const PORT = Number(process.env.PORT) || 3000 as number
+export const PORT = Number(process.env.PORT) || (3000 as number);
